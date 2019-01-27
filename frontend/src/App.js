9c0c3cd71eae6afaeb28d85ed247fc185ae5887c
jtisaac <jtisaac@users.noreply.github.com>
2019-01-27 02:09:59 -0600

    Finished React Part 5!

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,14 +11,31 @@ class App extends Component {
         {id: 1, name: "Game of Thrones", episodes_seen: 0},
         {id: 2, name: "Naruto", episodes_seen: 220},
         {id: 3, name: "Black Mirror", episodes_seen: 3},
-      ]
+      ],
+      nextId: 4
     }
   }
-
+  addShow = (event) => {
+  // thanks https://medium.com/@everdimension/how-to-handle-forms-with-just-react-ac066c48bd4f
+    event.preventDefault();
+    const data = new FormData(event.target);
+    var name = data.get('name').toString();
+    var currShows = this.state.shows;
+    currShows.push({id: this.state.nextId, name: name, episodes_seen: 0});
+    this.setState({ shows: currShows });
+    this.setState({ nextId: this.state.nextId + 1})
+  }
   render() {
     return (
       <div className="App">
         <Instructions complete={"yes"}/>
+        <form onSubmit={this.addShow}>
+          <label>
+            New Show Name:
+          </label>
+          <input type="text" name="name" ref={(input) => this.textInput = input} />
+          <button>Submit</button>
+        </form>
         {this.state.shows.map(x => (
           <Show id={x.id} name={x.name} episodes_seen={x.episodes_seen} />
         ))}
